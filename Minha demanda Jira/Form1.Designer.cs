48f5775855e07c5c9a6ea9388472c44bef207f79
rafaelSimoes_AeC <rafaelbonaldi@gmail.com>
2017-07-05 18:16:08 -0300

    first commit

---
+++ Minha demanda Jira/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Minha_demanda_Jira
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_entrar = new System.Windows.Forms.Button();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.txt_senha = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_entrar
+            // 
+            this.btn_entrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_entrar.Location = new System.Drawing.Point(339, 24);
+            this.btn_entrar.Name = "btn_entrar";
+            this.btn_entrar.Size = new System.Drawing.Size(96, 34);
+            this.btn_entrar.TabIndex = 0;
+            this.btn_entrar.Text = "Entrar";
+            this.btn_entrar.UseVisualStyleBackColor = true;
+            this.btn_entrar.Click += new System.EventHandler(this.btn_entrar_Click);
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_usuario.Location = new System.Drawing.Point(13, 37);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(160, 20);
+            this.txt_usuario.TabIndex = 1;
+            // 
+            // txt_senha
+            // 
+            this.txt_senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_senha.Location = new System.Drawing.Point(192, 37);
+            this.txt_senha.Name = "txt_senha";
+            this.txt_senha.Size = new System.Drawing.Size(131, 20);
+            this.txt_senha.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(189, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Senha";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_entrar);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_usuario);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txt_senha);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(4, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(442, 70);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 397);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_entrar;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.TextBox txt_senha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Minha demanda Jira/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SHDocVw;
+using System.Runtime.InteropServices;
+using mshtml;
+using System.Collections;
+
+namespace Minha_demanda_Jira
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    [Guid("D40C654D-7C51-4EB3-95B2-1E23905C2A2D")]
+    
+    public partial class Form1 : Form
+    {
+        static InternetExplorer ie;
+        private const string UrlLogin = "http://jira:8080/login.jsp";
+        string Metodo = "Login";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_entrar_Click(object sender, EventArgs e)
+        {
+            CarregaDados();
+        }
+
+        private void CarregaDados()
+        {
+            Metodo = "Login";
+
+            ie = new InternetExplorer() { Visible = true, };
+
+            ie.Navigate2(UrlLogin);
+
+            ie.DocumentComplete += Ie_DocumentComplete;
+        }
+
+        private void Ie_DocumentComplete(object pDisp, ref object URL)
+        {
+            switch (Metodo)
+            {
+                case "Login":
+                    login();
+                    break;
+                case "ValidarTelaInicial":
+                    ValidarTelaInicial();
+                    break;
+                case "PegarValor":
+                    PegarValor();
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+        }
+
+        public void login()
+        {
+            Metodo = "ValidarTelaInicial";
+
+            var document = ie.Document as IHTMLDocument3;
+
+            var User = document.getElementById("login-form-username");
+            User.innerText = txt_usuario.Text;
+            User.style.border = "1px solid #e01e1e";
+
+            var password = document.getElementById("login-form-password");
+            password.innerText = txt_senha.Text;
+            password.style.border = "1px solid #e01e1e";
+
+            var SignIn = document.getElementsByName("login");
+            var login = (IHTMLElement)SignIn.item(0);
+            login.click();
+        }
+
+        public void ValidarTelaInicial()
+        {
+            Metodo = "PegarValor";
+
+            var document = ie.Document as IHTMLDocument3;
+
+            var Dashboard = document.getElementById("dashboard");
+
+            if (Dashboard != null)
+            {
+                document.getElementById("greenhopper_menu").click();
+                document.getElementById("rapidb_lnk_132_lnk").click();
+            }
+        }
+
+        public void PegarValor()
+        {
+            var document = ie.Document as IHTMLDocument3;
+
+            var a = document.getElementById("ghx-column-headers");
+        }
+    }
+}
